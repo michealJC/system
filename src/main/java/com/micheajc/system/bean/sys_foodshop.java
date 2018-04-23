0904b8cba702c426e2b2a65646b2afcbe933605e
acer <958231955@qq.com>
2018-04-23 08:30:55 +0800

    测试templates

---
+++ src/main/java/com/micheajc/system/bean/sys_foodshop.java
@@ -0,0 +1,53 @@
+package com.micheajc.system.bean;
+
+public class sys_foodshop {
+
+    private String image;
+    private String foodname;
+    private String foodjiage;
+    private String foodshuliang;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getFoodname() {
+        return foodname;
+    }
+
+    public void setFoodname(String foodname) {
+        this.foodname = foodname;
+    }
+
+    public String getFoodjiage() {
+        return foodjiage;
+    }
+
+    public void setFoodjiage(String foodjiage) {
+        this.foodjiage = foodjiage;
+    }
+
+    public String getFoodshuliang() {
+        return foodshuliang;
+    }
+
+    public void setFoodshuliang(String foodshuliang) {
+        this.foodshuliang = foodshuliang;
+    }
+
+    public sys_foodshop(String image, String foodname, String foodjiage, String foodshuliang) {
+
+        this.image = image;
+        this.foodname = foodname;
+        this.foodjiage = foodjiage;
+        this.foodshuliang = foodshuliang;
+    }
+
+    public sys_foodshop() {
+
+    }
+}
---
+++ src/main/java/com/micheajc/system/controller/Foodshop_controller.java
@@ -0,0 +1,15 @@
+package com.micheajc.system.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/foodshop")
+public class Foodshop_controller {
+
+    @RequestMapping("/foodshopmangger")
+    public String getshop(){
+        return "shopCart";
+    }
+}
