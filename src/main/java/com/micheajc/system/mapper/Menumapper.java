c34b8bedea2b1586431bea065af28ecf0ca54de5
acer <958231955@qq.com>
2018-05-04 14:36:40 +0800

    mymenu3

--- src/main/java/com/micheajc/system/mapper/Menumapper.java
+++ src/main/java/com/micheajc/system/mapper/Menumapper.java
@@ -1,6 +1,7 @@
 package com.micheajc.system.mapper;
 
 
+import com.micheajc.system.bean.SysCarryFood;
 import com.micheajc.system.bean.SysMenuTable;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -18,4 +19,8 @@ public interface Menumapper {
     //得到所有桌子是否被选中
     @Select("select renshu,addjine,data,id from sys_tablenumber where zhuangtai=0")
     public List<SysMenuTable> getMenuTable();
+
+    //得到所有相同菜品的菜名和份数
+    @Select("SELECT foodname,SUM(foodshuliang) AS foodshuliang  FROM sys_foodcarryshop GROUP BY foodname")
+    public List<SysCarryFood> getcarryfood();
 }
